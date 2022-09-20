8133e29e38c870f9fd57e3eee05e98c94ad2c84e
Peter Ayeni <ayenicology@gmail.com>
2022-09-20 14:28:08 +0100

    configure code generator

--- schema/index.ts
+++ schema/index.ts
@@ -1,7 +1,8 @@
 import { importSchema } from 'graphql-import';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { makeExecutableSchema} from '@graphql-tools/schema';
+import {IResolvers} from '@graphql-tools/utils'
 import resolvers from './resolvers';
 
 const typeDefs = importSchema('schema/typeDefs.graphql');
 
-export default makeExecutableSchema({ resolvers, typeDefs });
+export default makeExecutableSchema({ resolvers: resolvers as IResolvers, typeDefs });
